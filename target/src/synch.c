c1a028f21267e9a7707a64ba9f2d4e31f06f6c54
Flynn Underwood <fun15@uclive.ac.nz>
2024-08-13 13:52:45 +1200

    - Removed button/switch queues - Added accelerometer queue

--- target/src/synch.c
+++ target/src/synch.c
@@ -3,13 +3,11 @@
 // Define semaphore handles
 SemaphoreHandle_t xADCSemaphore; // ADC signalling semaphore
 SemaphoreHandle_t xDeviceStateMutex; // deviceState shared resource protection
-QueueHandle_t button_q;
-QueueHandle_t switch_q;
+QueueHandle_t accl_q;
 
 void createSemaphores(void) {
     // Create semaphores, mutexes, and queues
     xADCSemaphore = xSemaphoreCreateBinary();
     xDeviceStateMutex = xSemaphoreCreateMutex();
-    button_q = xQueueCreate(10, sizeof(enum butNames));
-    switch_q = xQueueCreate(10, sizeof(enum SWStates));
+    accl_q = xQueueCreate(1, BUF_SIZE);
 }
--- target/src/synch.h
+++ target/src/synch.h
@@ -5,14 +5,12 @@
 #include "task.h"
 #include "semphr.h"
 #include "queue.h"
-#include "buttons4.h"
-#include "switches.h"
+#include "accl_manager.h"
 
 // Declare semaphore handles
 extern SemaphoreHandle_t xADCSemaphore;
 extern SemaphoreHandle_t xDeviceStateMutex;
-extern QueueHandle_t button_q;
-extern QueueHandle_t switch_q;
+extern QueueHandle_t accl_q;
 
 void createSemaphores(void);
 
